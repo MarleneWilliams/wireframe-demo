187377366cf6fc5fbcbe982d81d538f51693f0a7
Marlene Williams <marlene@designbymarlene.com>
2013-08-09 01:52:41 -0700

    Many content and style changes.

---
+++ js/custom/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  // // Mobile dropdown buttons
+  // $('#header #block-search-form').prepend('<div class="dropdown-icon">Search</div>');
+
+  // // Toggle dropdowns on click
+  // if($('div.dropdown-icon').length) {
+  //   var toggleLink = $('div.dropdown-icon');
+  //   toggleLink.click(function() {
+  //      // $($(this).parent()).find('ul.menu:first').toggleClass("xactive");
+  //      $($(this).parent()).find('form:first').slideToggle('0');
+  //      console.log("it has been clicked");
+  //   });
+  // }
+
+
+});
