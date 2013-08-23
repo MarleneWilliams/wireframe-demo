ded6e23bfef68df91a1bc9fb58b3620081201699
Marlene Williams <marlene@designbymarlene.com>
2013-08-22 19:16:36 -0700

    Refactoring a little plus adding in home page legend.

---
+++ wireframes_dev/legend/01_home.php
@@ -0,0 +1,36 @@
+
+elms: [
+  {sel:'.logo-link',idx:1, styles:{top: '3em', left:'2em'},
+    notes:'<span class="name">Logo, links to the home page</span>'
+  },
+  {sel:'#block-menu-menu-audience-menu', idx:2, styles:{left:'95%'},
+    notes:'<span class="name">Audience menu, allowing visitors to get find info based on who they are.</span>'
+  },
+  {sel:'#block-search-form', idx:3, parentStyles:{overflow:'visible'},
+    notes:'<span class="name">Simple search of the site, takes visitors to a search results page</span>'
+  },
+  {sel:'.primary-menu', idx:4, parentStyles:{overflow:'visible'},
+    notes:'<span class="name">The main, topical navigation menu for the site. All pages should have a spot in here.</span>'
+  },
+  {sel:'#slider', idx:5,
+    notes:'<span class="name">Slideshow, which can contain announcements, information and calls to action. Slides can contain text in addition to the images.</span>'
+  },
+  {sel:'.cta-item', idx:6,
+    notes:'<span class="name">Calls to action - Images with optional text directing visitors to highlighted areas of the site.</span>'
+  },
+  {sel:'#block-view-upcoming-events', idx:7,
+    notes:'<span class="name">Upcoming events list, linking to individual event pages as well as a link to the full calendar.</span>'
+  },
+  {sel:'.pane-news-panel-pane-2', idx:8,
+    notes:'<span class="name">Recent news and announcements, linking to the individual announcement pages.</span>'
+  },
+  {sel:'#block-menu-menu-footer-menu', idx:9,
+    notes:'<span class="name">Footer menu, for displaying organized links that should show on every page.</span>'
+  },
+  {sel:'#block-menu-menu-social-links', idx:10,
+    notes:'<span class="name">Social media links, directing to the college social network pages</span>'
+  },
+  {sel:'#block-block-1', idx:11, styles:{left:'95%'},
+    notes:'<span class="name">Footer text, appropriate for persistent static info that should appear on every page.</span>'
+  }
+]
